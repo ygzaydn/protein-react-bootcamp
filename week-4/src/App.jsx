9236f8588b03fc4a80d0ecc881205712a340d3d8
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2022-04-03 10:43:29 +0300

    week4 init

---
+++ week-4/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            asdasdasd
+            <span>asd</span>
+            <div>sadasd</div>
+        </div>
+    );
+}
+
+export default App;
