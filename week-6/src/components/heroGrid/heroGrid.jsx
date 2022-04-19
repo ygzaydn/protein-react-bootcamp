f4a5d65db535f17331f6135bd0f7e5a3bfb90dbd
erolyagizaydin <32926423+ygzaydn@users.noreply.github.com>
2022-04-19 23:49:21 +0300

    finalized

--- week-6/src/components/heroGrid/heroGrid.jsx
+++ week-6/src/components/heroGrid/heroGrid.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import axios from "../../constants/axios";
 
 import { useParams } from "react-router";
+import { useTranslation } from "react-i18next";
 
 const HeroGrid = ({ chosen, setChosen }) => {
   const { id } = useParams();
   const idToUse = id ? id.split(":")[1] : null;
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (!chosen?.id && idToUse && idToUse !== chosen?.id) {
@@ -40,29 +42,27 @@ const HeroGrid = ({ chosen, setChosen }) => {
             <h2 className="hero__name">{chosen.name}</h2>
 
             <h4 className="hero__description">
-              {chosen.description
-                ? chosen.description
-                : "No available description"}
+              {chosen.description ? chosen.description : t("heroPage.nodesc")}
             </h4>
             <div className="hero__additionalDiv">
               <div className="hero__seriesDiv">
-                <h4>Series</h4>
+                <h4>{t("heroPage.series")}</h4>
                 {chosen.series.available ? (
                   chosen.series.items.map(
                     (el, ind) => ind < 5 && <span key={el.name}>{el.name}</span>
                   )
                 ) : (
-                  <h5>No available series</h5>
+                  <h5>{t("heroPage.noseries")}</h5>
                 )}
               </div>
               <div className="hero__storiesDiv">
-                <h4>Stories</h4>
+                <h4>{t("heroPage.stories")}</h4>
                 {chosen.stories.available ? (
                   chosen.stories.items.map(
                     (el, ind) => ind < 5 && <span key={el.name}>{el.name}</span>
                   )
                 ) : (
-                  <h5>No available story</h5>
+                  <h5>{t("heroPage.nostory")}</h5>
                 )}
               </div>
             </div>
